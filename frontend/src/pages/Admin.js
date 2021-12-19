271bbea3bb64af5f9d3cfbe8b5c3569b44a96bcc
ag502 <ag502@naver.com>
2021-12-19 22:39:35 +0900

    Add : logout button

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,11 +1,15 @@
 import { useState, useEffect, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 import { requestAllUsers } from "../common/api/admin";
+import { removeTokenToLocalStorage } from "../common/manageToken";
 import { parseDashedDate } from "../common/utilFunc";
 
 function Admin() {
   const [accounts, setAccounts] = useState([]);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     getAllUsers();
@@ -18,31 +22,43 @@ function Admin() {
     } catch (err) {}
   }, []);
 
+  const handleLogoutClick = () => {
+    removeTokenToLocalStorage();
+    navigate("/");
+  };
+
   return (
-    <Table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Account</th>
-          <th>가입일</th>
-        </tr>
-      </thead>
-      <tbody>
-        {accounts.map((account) => (
-          <tr key={account.id}>
-            <TableCell>{account.id}</TableCell>
-            <TableCell>{account.name}</TableCell>
-            <TableCell>{parseDashedDate(account.createdAt)}</TableCell>
+    <AdminPageContainer>
+      <Table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Account</th>
+            <th>가입일</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {accounts.map((account) => (
+            <tr key={account.id}>
+              <TableCell>{account.id}</TableCell>
+              <TableCell>{account.name}</TableCell>
+              <TableCell>{parseDashedDate(account.createdAt)}</TableCell>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <LogoutLink onClick={handleLogoutClick}>로그아웃</LogoutLink>
+    </AdminPageContainer>
   );
 }
 
-const Table = styled.table`
+const AdminPageContainer = styled.div`
   width: 90%;
   margin: 20px auto 0;
+`;
+
+const Table = styled.table`
+  width: 100%;
   background-color: #ffffff;
   border-radius: 10px;
   border-collapse: collapse;
@@ -51,6 +67,15 @@ const Table = styled.table`
 const TableCell = styled.td`
   text-align: center;
   border-top: 1px solid #000000;
+`;
+
+const LogoutLink = styled.div`
+  margin-top: 10px;
+  text-align: right;
+  color: #0aaf9e;
+  font-weight: bold;
+  text-decoration: underline;
+  cursor: pointer;
 `;
 
 export default Admin;
