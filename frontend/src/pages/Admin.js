9720637895ac2720cbf518c12b3b475e59497bb3
ag502 <ag502@naver.com>
2021-12-20 17:23:09 +0900

    Fix : unAuthorized page

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,18 +1,45 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
+import { requestMessage } from "../common/constants";
 import { requestAllUsers } from "../common/api/admin";
+import { checkLogin } from "../common/api/auth";
 import { removeTokenToLocalStorage } from "../common/manageToken";
 import { parseDashedDate } from "../common/utilFunc";
+import { SnackBar } from "../components/common";
 
 function Admin() {
   const [accounts, setAccounts] = useState([]);
+  const [snackBarOpen, setSnackBarOpen] = useState(false);
+  const [snackBarMsg, setSnackBarMsg] = useState("");
+  const isMounted = useRef(false);
 
   const navigate = useNavigate();
 
   useEffect(() => {
-    getAllUsers();
+    checkAuth();
+  }, []);
+
+  useEffect(() => {
+    if (isMounted.current && !snackBarOpen) {
+      navigate("/");
+    }
+    isMounted.current = true;
+  }, [snackBarOpen]);
+
+  const checkAuth = useCallback(async () => {
+    try {
+      const { userData } = await checkLogin();
+
+      if (userData.roll === "general") {
+        throw requestMessage.NOT_ADMIN;
+      }
+      getAllUsers();
+    } catch (err) {
+      setSnackBarOpen(true);
+      setSnackBarMsg(err);
+    }
   }, []);
 
   const getAllUsers = useCallback(async () => {
@@ -27,28 +54,43 @@ function Admin() {
     navigate("/");
   };
 
+  const handleSnackBarClose = () => {
+    setSnackBarOpen(false);
+  };
+
   return (
-    <AdminPageContainer>
-      <Table>
-        <thead>
-          <tr>
-            <TableHeader>ID</TableHeader>
-            <TableHeader>Account</TableHeader>
-            <TableHeader>가입일</TableHeader>
-          </tr>
-        </thead>
-        <tbody>
-          {accounts.map((account) => (
-            <TableRow key={account.id}>
-              <TableCell>{account.id}</TableCell>
-              <TableCell>{account.name}</TableCell>
-              <TableCell>{parseDashedDate(account.createdAt)}</TableCell>
-            </TableRow>
-          ))}
-        </tbody>
-      </Table>
-      <LogoutLink onClick={handleLogoutClick}>로그아웃</LogoutLink>
-    </AdminPageContainer>
+    <>
+      <SnackBar
+        open={snackBarOpen}
+        message={snackBarMsg}
+        onClose={handleSnackBarClose}
+        autoHideDuration={700}
+        severity='error'
+      />
+      {!snackBarOpen && (
+        <AdminPageContainer>
+          <Table>
+            <thead>
+              <tr>
+                <TableHeader>ID</TableHeader>
+                <TableHeader>Account</TableHeader>
+                <TableHeader>가입일</TableHeader>
+              </tr>
+            </thead>
+            <tbody>
+              {accounts.map((account) => (
+                <TableRow key={account.id}>
+                  <TableCell>{account.id}</TableCell>
+                  <TableCell>{account.name}</TableCell>
+                  <TableCell>{parseDashedDate(account.createdAt)}</TableCell>
+                </TableRow>
+              ))}
+            </tbody>
+          </Table>
+          <LogoutLink onClick={handleLogoutClick}>로그아웃</LogoutLink>
+        </AdminPageContainer>
+      )}
+    </>
   );
 }
 
